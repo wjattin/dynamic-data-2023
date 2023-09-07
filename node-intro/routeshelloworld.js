86559ff870d4e462828308399bee7f4920fabcbd
Billy Jattin <bjattin@veteranmediagroup.com>
2023-09-07 19:59:34 -0400

    Added comments

--- node-intro/routeshelloworld.js
+++ node-intro/routeshelloworld.js
@@ -1,8 +1,13 @@
-//Setup our server 
-const http = require('http');
-//Define the port the app will be access from (80,8080,8888 are default to the domain /)
-const port = process.env.PORT || 3000;
+//Setup our server
 
+//Node.js has a built-in module called HTTP, which allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP)
+const http = require('http');
+//Define the port the app will be access from (80,8080,8888 are usually default for domains)
+//In our local environment we will use 3000,4000 or any number in between when another process is runnib on that port 
+const port = process.env.PORT || 3000;
+// The HTTP module can create an HTTP server that listens to server ports and gives a response back to the client.
+
+// Use the createServer() method to create an HTTP server:
 const server = http.createServer( (request,response) => {
 // Get the url 
 const path = request.url;
